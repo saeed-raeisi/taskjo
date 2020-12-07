a226e7f94b28e1b4ab82c7cc1b57b50a5eba4910
alireza <alireza.khoshneshin@gmail.com>
2020-12-08 00:57:42 +0330

    add migration

--- taskjo/Entities/TeamRule.cs
+++ taskjo/Entities/TeamRule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace taskjo.Models
 {
     public class TeamRule
     {
+        [Key]
         public int ruleId { get; set; }
         public string ruleName { get; set; }
 
